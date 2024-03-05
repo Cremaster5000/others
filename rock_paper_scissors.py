8afd553dae21ad5d8bc0b4a6d29aa68fffa281d7
Eduardo Tellez <tellezlalo94@gmail.com>
2024-03-05 12:01:04 -0600

    Some changes in rock paper scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,28 +2,35 @@ import random
 from os import system
 import time
 
+"""This script emules a rock, paper, scissors game generating
+    pseudo-random choices; it says if you win, lose or get draw.
+    It doesn't accept lower case first letter, can show you instructions
+    or just close the game. It's made as first step to a mobile game.
+    """
 
 
 instructions = "Type "'Exit'" to close game; type "'Rock'", "'Paper'", "'Scissors'" to play or "'help'" to view this sentence again. "
 options = {"Rock":"Paper", "Paper":"Scissors", "Scissors":"Rock"}
-commands = {"Exit": system('exit'), "Help":instructions} 
 
 def play():
     print(instructions)
     while True:
         player = input("Write your move:")
-        if player in commands: commands[player]
+        if player == "Exit": break
+        elif player == "Help": print(instructions)
         elif player in options: 
             result = draw()
-            if options[player] == result: print("Ganaste")
-            elif options[result] == player: print("Perdiste")
-            else: print("Empate")
-        else: print("Opción no valida")
-        time.sleep(2)
+            if options[player] == result: print("You lost")
+            elif options[result] == player: print("You won")
+            else: print("Draw")
+        else: print("Invalid option")
+        time.sleep(3)
+        system('clear')
             
 def draw():
     global options
-    selection = random.choice(options.keys())
+    selection = random.choice(list(options.keys()))
+    print(selection)
     return selection
 
 play()
